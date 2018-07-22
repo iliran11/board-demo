26503ab0c41a21235f83c9894536fa262b12a15a
Liran Cohen <iliran11@gmail.com>
2018-07-22 18:53:31 +0300

    dragging logic - wip

--- src/draghelpers.js
+++ src/draghelpers.js
@@ -1,3 +1,24 @@
-export function itemResort(result,state) {
-  console.log(result)
+import has from "lodash.has";
+
+export function itemResort(result, state) {
+  // const nextState = { ...state };
+  // const { destination, source } = result;
+  // const sourceSection = source.droppableId;
+  // const destinationSection = destination.droppableId;
+  // const sourceList = getListOfSection(nextState, sourceSection);
+  // const destinationList = getListOfSection(nextState, destinationSection);
+  // const draggedItem = result.draggableId;
+
+  // [removed] = nextState[sourceList][sourceSection].splice(source.index);
+  // console.log(result);
+}
+
+function getListOfSection(state, sectionId) {
+  const stateKeys = Object.keys(state);
+  const result = stateKeys.filter(listKey => {
+    const arrayOfSections = state[listKey];
+    return arrayOfSections.some(section => {
+      return section.id === sectionId;
+    });
+  });
 }
